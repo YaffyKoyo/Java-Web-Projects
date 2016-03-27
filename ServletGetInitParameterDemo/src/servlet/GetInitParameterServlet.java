bd408d7339407dccf71655dc7feee7ed3b1436d4
YaffyKoyo <qyaffy@gmail.com>
2016-03-27 19:07:19 +0100

    initialparameter

---
+++ ServletGetInitParameterDemo/src/servlet/GetInitParameterServlet.java
@@ -0,0 +1,103 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetInitParameterServlet
+ */
+@WebServlet("/GetInitParameterServlet")
+public class GetInitParameterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private String username;//
+	private String password;
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username
+	 *            the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * Default constructor.
+	 */
+	public GetInitParameterServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		this.setUsername(config.getInitParameter("username"));
+		this.setPassword(config.getInitParameter("password"));
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>A servlet</title><head>");
+		out.println("<body>");
+		out.println("<h2>"+"Username: "+this.getUsername()+"</h2>");
+		out.println("<h2>"+"Password: "+this.getPassword()+"</h2>");		
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
